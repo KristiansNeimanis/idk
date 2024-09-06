f447158a41001590cbe780dafe45887207b6763e
unknown <your@email.example>
2024-09-06 09:34:39 +0300

    first commit

--- main.sql
+++ main.sql
@@ -1,6 +1,10 @@
 -- use sakila;
-
 -- select * from address;
 
-use sql_store;
-select * from customer;
+-- use sql_store;
+select 
+* 
+from customers
+-- where first_name = "Elka";
+order by customer_id;
+select 5*5, "result"
---
+++ select/2.sql
@@ -0,0 +1,9 @@
+-- uzdevums: atlasīt datus no tabulas 'customers'
+-- datubāze 'sql_store'
+-- atlasīt vārdu, uzvārdu un punktus
+
+-- use sql_store;
+-- select first_name, last_name, points, points+100 as "points_increased"
+-- from customers;
+
+select state from customers;
