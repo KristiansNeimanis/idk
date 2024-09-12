5dd4d0ac49aebf8a35082177179fcd7884e16a55
Kristians <test@email.com>
2024-09-12 10:50:50 +0300

    .\.git\

--- 2select-join/4.sql
+++ 2select-join/4.sql
@@ -15,3 +15,12 @@ left join orders o on o.customer_id = c.customer_id;
 use sql_store;
 select * from customers c
 right join orders o on o.customer_id = c.customer_id;
+
+
+-- uzdevums
+-- atlasīt produktus un pasūtījuma vienības tā lai tiktu atlasīti visi produkti
+-- arī tādi kuri nav pasūtīti
+
+use sql_store;
+select * from products p
+left join order_items oi on oi.product_id = p.product_id;
